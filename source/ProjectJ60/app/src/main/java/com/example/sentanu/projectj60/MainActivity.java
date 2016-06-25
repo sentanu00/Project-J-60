5327b89a0aa6bb073e710f8e7f1daf0c42d27c41
Sentanu <sentanu00@gmail.com>
2016-06-25 22:52:23 +0700

    google map jalan dan masih polosdari main ke google map

--- source/ProjectJ60/app/src/main/java/com/example/sentanu/projectj60/MainActivity.java
+++ source/ProjectJ60/app/src/main/java/com/example/sentanu/projectj60/MainActivity.java
@@ -1,17 +1,28 @@
 package com.example.sentanu.projectj60;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btn_map;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_map = (Button)findViewById(R.id.btn_map);
+        btn_map.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i;
+                i = new Intent(MainActivity.this, MapsActivity.class);
+                startActivity(i);
+            }
+        });
     }
 
-public void cobafungsi(){
-    int xx = 10;
-}
 }
