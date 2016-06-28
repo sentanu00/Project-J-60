c38b31193ac254a21c41888f36de86480902f60c
Sentanu <sentanu00@gmail.com>
2016-06-28 14:27:48 +0700

    gritview ke map sudah, namun masih lag dan belum ambil image dari database

--- source/ProjectJ60/app/src/main/java/com/example/sentanu/projectj60/MainActivity.java
+++ source/ProjectJ60/app/src/main/java/com/example/sentanu/projectj60/MainActivity.java
@@ -1,10 +1,17 @@
 package com.example.sentanu.projectj60;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
 import android.widget.Button;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,7 +21,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btn_map = (Button)findViewById(R.id.btn_map);
+        GridView gridView= (GridView)findViewById(R.id.gridview);
+        gridView.setAdapter(new ImageAdapter(this));
+
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, ""+position, Toast.LENGTH_SHORT).show();
+
+                //disini get posisi dari gritview set kordinat dari database dan lempar ke map
+                
+                Intent i;
+                i = new Intent(MainActivity.this, MapsActivity.class);
+                startActivity(i);
+            }
+        });
+
+      /*  btn_map = (Button)findViewById(R.id.btn_map);
         btn_map.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -22,7 +45,77 @@ public class MainActivity extends AppCompatActivity {
                 i = new Intent(MainActivity.this, MapsActivity.class);
                 startActivity(i);
             }
-        });
+        });*/
+    }
+
+    public class ImageAdapter extends BaseAdapter{
+        private Context mContext;
+
+        public ImageAdapter(Context c){
+            mContext=c;
+        }
+
+        private Integer[] mThumbIds = {
+                R.drawable.sample_1,
+                R.drawable.sample_1,
+                R.drawable.sample_3,
+                R.drawable.sample_1,
+                R.drawable.sample_1,
+                R.drawable.sample_1,
+                R.drawable.sample_1,
+                R.drawable.sample_3,
+                R.drawable.sample_1,
+                R.drawable.sample_1,
+                R.drawable.sample_1,
+                R.drawable.sample_1,
+                R.drawable.sample_3,
+                R.drawable.sample_1,
+                R.drawable.sample_1,
+                R.drawable.sample_1,
+                R.drawable.sample_1,
+                R.drawable.sample_3,
+                R.drawable.sample_1,
+                R.drawable.sample_1,
+                R.drawable.sample_1,
+                R.drawable.sample_1,
+                R.drawable.sample_3,
+                R.drawable.sample_1,
+                R.drawable.sample_1,
+                R.drawable.sample_1,
+                R.drawable.sample_1,
+                R.drawable.sample_3,
+                R.drawable.sample_1,
+                R.drawable.sample_1,
+                R.drawable.sample_1,
+                R.drawable.sample_1,
+                R.drawable.sample_3,
+                R.drawable.sample_1,
+                R.drawable.sample_1,
+                R.drawable.sample_3
+        };
+        @Override
+        public int getCount() {
+            return mThumbIds.length;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ImageView imageView = new ImageView(mContext);
+            imageView.setLayoutParams(new GridView.LayoutParams(170,170));
+            imageView.setPadding(6,6,6,6);
+            imageView.setImageResource(mThumbIds[position]);
+            return imageView;
+        }
     }
 
 }
