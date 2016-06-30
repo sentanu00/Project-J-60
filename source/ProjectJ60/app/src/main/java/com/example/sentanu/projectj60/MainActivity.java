c87062ae84c49abd3817decd7705267981f83078
Sentanu <sentanu00@gmail.com>
2016-06-30 16:56:50 +0700

    1. bisa share data dari gridview ke map, 2. bisa fokus tampilan map 3. ada 2 radius namun geofance notifikasi belum aktif 4. belum konek ke database 5. tinggal aktifkan tombol kamera dan geofance

--- source/ProjectJ60/app/src/main/java/com/example/sentanu/projectj60/MainActivity.java
+++ source/ProjectJ60/app/src/main/java/com/example/sentanu/projectj60/MainActivity.java
@@ -1,10 +1,17 @@
 package com.example.sentanu.projectj60;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
 import android.widget.Button;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,7 +21,36 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btn_map = (Button)findViewById(R.id.btn_map);
+        GridView gridView= (GridView)findViewById(R.id.gridview);
+        gridView.setAdapter(new ImageAdapter(this));
+
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, ""+position, Toast.LENGTH_SHORT).show();
+
+                //disini get posisi dari gritview dan get kordinat, radius 1 radius 2 dari database dan lempar ke map
+                //disini harusnya yang di get adalah data dari database, bukan posisi gridview
+                Intent i;
+                i = new Intent(MainActivity.this, MapsActivity.class);
+                i.putExtra("nama_quest", "Quest 1");
+
+                //catatan untuk radius atau apapun ber ekstensi "Double" bila akan di share melalui
+                // intent, covert ke "String" terlebih dahulu seperti latRadius dan lonRadius di
+                // bawah ini
+                i.putExtra("latRadius", "-6.9295007");
+                i.putExtra("lonRadius", "107.6080578");
+                i.putExtra("radRadius", 100);
+                i.putExtra("latTarget", "-6.9295007");
+                i.putExtra("lonTarget", "107.6080578");
+                i.putExtra("radTarget", 40);
+
+                startActivity(i);
+            }
+        });
+
+
+        /*btn_map = (Button)findViewById(R.id.btn_map);
         btn_map.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -22,7 +58,66 @@ public class MainActivity extends AppCompatActivity {
                 i = new Intent(MainActivity.this, MapsActivity.class);
                 startActivity(i);
             }
-        });
+        });*/
     }
+
+
+    public class ImageAdapter extends BaseAdapter {
+        private Context mContext;
+
+        public ImageAdapter(Context c){
+            mContext=c;
+        }
+
+        private Integer[] mThumbIds = {
+                R.mipmap.sample_1,
+                R.mipmap.sample_1,
+                R.mipmap.sample_1,
+                R.mipmap.sample_1,
+                R.mipmap.sample_1,
+                R.mipmap.sample_1,
+                R.mipmap.sample_1,
+                R.mipmap.sample_1,
+                R.mipmap.sample_1,
+                R.mipmap.sample_1,
+                R.mipmap.sample_1,
+                R.mipmap.sample_1,
+                R.mipmap.sample_1,
+                R.mipmap.sample_1,
+                R.mipmap.sample_1,
+                R.mipmap.sample_1,
+                R.mipmap.sample_1,
+                R.mipmap.sample_1,
+                R.mipmap.sample_1,
+                R.mipmap.sample_1,
+                R.mipmap.sample_1
+        };
+        @Override
+        public int getCount() {
+            return mThumbIds.length;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            //disini pengaturan tampilan griview
+            ImageView imageView = new ImageView(mContext);
+            imageView.setLayoutParams(new GridView.LayoutParams(170,170));
+            imageView.setPadding(6,6,6,6);
+            imageView.setImageResource(mThumbIds[position]);
+            return imageView;
+        }
+    }
+
+
 
 }
--- source/ProjectJ60/app/src/main/java/com/example/sentanu/projectj60/MapsActivity.java
+++ source/ProjectJ60/app/src/main/java/com/example/sentanu/projectj60/MapsActivity.java
@@ -1,16 +1,35 @@
 package com.example.sentanu.projectj60;
 
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+
+    String namaQuest, latRadius, longiRadius, latTarget, longiTarget;
+    LatLng radius, target;
+    int radRadius, radTarget;
+    Button GTTarget, GTMyloc;
+    LatLng mylocation;//-----------------------------------------------------------------lokasi user
+    private LocationManager locManager;
 
     private GoogleMap mMap;
 
@@ -22,6 +41,68 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        GTTarget = (Button)findViewById(R.id.btn_go_to_target);
+        GTMyloc = (Button)findViewById(R.id.btn_go_to_myloc);
+
+        //---------------------------------------------------------------------------------------------- ambil lokasi user
+        locManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        Location location =  locManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        mylocation = new LatLng(location.getLatitude(),location.getLongitude());
+        //----------------------------------------------------------------------------------------------
+        //set button search target on---------------------------------------------------------------
+        GTTarget.setVisibility(View.GONE);
+        GTMyloc.setVisibility(View.VISIBLE);
+
+        GTTarget.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                GTTarget.setVisibility(View.GONE);
+                GTMyloc.setVisibility(View.VISIBLE);
+                setingKamera(target);
+            }
+        });
+        GTMyloc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                GTTarget.setVisibility(View.VISIBLE);
+                GTMyloc.setVisibility(View.GONE);
+                setingKamera(mylocation);
+            }
+        });
+
+
+        //ambil data yang dikirim dari MainActovoty.java
+        Bundle extras = getIntent().getExtras();
+        this.namaQuest = extras.getString("nama_quest");
+
+        this.latRadius = extras.getString("latRadius");
+        this.longiRadius = extras.getString("lonRadius");
+        this.radRadius = extras.getInt("radRadius");
+
+        this.latTarget = extras.getString("latTarget");
+        this.longiTarget = extras.getString("lonTarget");
+        this.radTarget = extras.getInt("radTarget");
+
+        //conversi lat long target dan radius yang berupa String ke LatLng
+        this.radius = new LatLng(Double.parseDouble(latRadius), Double.parseDouble(longiRadius));
+        this.target = new LatLng(Double.parseDouble(latTarget), Double.parseDouble(longiTarget));
+
+
+        Toast.makeText(MapsActivity.this, "nama quest : "+namaQuest, Toast.LENGTH_SHORT).show();
+        Toast.makeText(MapsActivity.this, "lat Radius : "+latRadius, Toast.LENGTH_SHORT).show();
+        Toast.makeText(MapsActivity.this, "Lon Radius : "+longiRadius, Toast.LENGTH_SHORT).show();
+        Toast.makeText(MapsActivity.this, "rad Radius : "+radRadius, Toast.LENGTH_SHORT).show();
+        Toast.makeText(MapsActivity.this, "lat Target : "+latTarget, Toast.LENGTH_SHORT).show();
+        Toast.makeText(MapsActivity.this, "Lon Target : "+longiTarget, Toast.LENGTH_SHORT).show();
+        Toast.makeText(MapsActivity.this, "rad Target : "+radTarget, Toast.LENGTH_SHORT).show();
+        Toast.makeText(MapsActivity.this, "radius : "+radius, Toast.LENGTH_SHORT).show();
+        Toast.makeText(MapsActivity.this, "target : "+target, Toast.LENGTH_SHORT).show();
+
+        /* radius adalah variabel untuk lingkaran geofence POI secara meluas atau tidak mendetail
+        * target adalah variabel untuk lokasi detil POI*/
+
+
     }
 
 
@@ -38,9 +119,95 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        //non aktifkan tombol mylocation pojok kanan atas
+        mMap.getUiSettings().setMyLocationButtonEnabled(false);
+
+        //cek permission gps
+        checkPermisionMyLocation();
+
+        //setting kamera
+        setingKamera(this.target);
+
+        //radius luar
+        buatLingkaran(radius,radRadius);
+
+        //radius dalam
+        //harusnya yang ini  tidak prlu di gambarkan, namun hanya perlu berupa notifikasi
+        // bahwa anda telah berada di tempat yg benar
+        buatLingkaran(target,radTarget);
+
+        //buat marker radius
+        // buatMarker(radius, R.mipmap.sample_marker_48, namaQuest);;
+    }
+
+
+    //fungsi cek permision my location pada manifest, dan bila
+    public void checkPermisionMyLocation(){
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            mMap.setMyLocationEnabled(true);
+        } else {
+            // Show rationale and request permission.
+        }
+    }
+
+    //fungsi ini bertujuan untuk perpindahan tampilan marker entah itu my location atau posisi POI
+    public void setingKamera(LatLng target){
+
+        //aktifkan zoom in pada kamera
+        mMap.animateCamera(CameraUpdateFactory.zoomIn());
+
+        //lakukan animasi zoom in ke level 10, dengan animasi durasi 2 detik
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(4),2000,null);
+
+        //mengatur fokus pada tampilan target
+        CameraPosition cm = new CameraPosition.Builder()
+                .target(target)     //sementara target ada di luar
+                .zoom(15)
+                .tilt(30)
+                .build();
+        mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cm));
+    }
+
+    //buat marker niatnya mau dirubah ke custom snippet agar waktu marker di klik
+    //muncul gambar dan keterangan
+    public void buatMarker(LatLng target, int lks_mrkr_img, String nama_marker){
+        mMap.addMarker(new MarkerOptions()
+                .position(target)
+                .title(nama_marker)
+                .snippet("Population: 4,137,400")
+                .icon(BitmapDescriptorFactory.fromResource(lks_mrkr_img)));
+    }
+
+    //disini untuk mevisualisasikan radius dalam POI
+    public void buatLingkaran(LatLng target, int radius){
+
+        CircleOptions circleOptions = new CircleOptions()
+                .center(new LatLng(target.latitude,target.longitude))
+                .radius(radius)// In meters
+                .strokeColor(0xffff9914)
+                .fillColor(0x40eeff00)
+                .strokeWidth(1);
+
+        mMap.addCircle(circleOptions);
+
+    }
+
+    //bila posisi device berada dalam radius POI maka button bisa di klik, begitu juga sebaliknya.
+    public Boolean set_button_camera_in_radius(LatLng myLoc, LatLng targetLoc){
+        Location loc1 = new Location("");
+        loc1.setLatitude(myLoc.latitude);
+        loc1.setLongitude(myLoc.longitude);
+        Location loc2 = new Location("");
+        loc2.setLatitude(targetLoc.latitude);
+        loc2.setLongitude(targetLoc.longitude);
+
+        float distanceInMeters = loc1.distanceTo(loc2); //---------------------------------- jarak dalam meter
+        if(distanceInMeters<100){
+            return true;
+        }else {
+            return false;
+        }
+
     }
 }
